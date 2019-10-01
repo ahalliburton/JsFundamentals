b4db0d35295577dcdf7b3cc74c6db09d23acde13
Allan Halliburton <allanmhalli@gmail.com>
2019-10-01 09:51:34 -0400

    first commit

---
+++ 01-grammarandtypes/01-declarations.js
@@ -0,0 +1,42 @@
+What are variables? Variables are named containers for storing data values.
+
+We name the variables so that we can refer to this data again.
+
+let a =2; 
+
+let b  =  1      
+/*(1) (2) (3) (4)
+
+1-Keyword
+2-Variable name
+3-Assignment Operater
+4-Variable Value
+*/
+
+Declarations are the left side of the variable:
+It is simply the var x
+It is on the left side of the assignment operator
+
+Initializations are the right side of a variable
+It sets the value of the variable
+It incorporates the variable name(x), the assignment operator (=), and the value (10)=> x =10
+(=) assignment operatoris not the same as the equal operator.Variables
+
+let x;
+console.log('Declaration:', x);
+
+x = 10;
+console.log('Initializing 1:', x);
+
+x = 33;
+console.log('Initializing 2:', x);
+
+let y = 'Hello';
+console.log('Both:', x, y);
+
+let today = 'Great!';
+const elevenFifty = 'Wonderful';
+console.log(today, elevenFifty);
+
+elevenFifty = 'Superr';
+console.log()
---
+++ 01-grammarandtypes/02-types.js
@@ -0,0 +1,126 @@
+//Boolean
+//boolean can represent true/false; yes/no
+
+let i = true;
+let j = false;
+
+console.log(i);
+
+//Null
+//null is an empty vale. it is not 0 or undefined
+//it is like an empty container; nothing is in it, but it still exista s a space to fil
+let empty = null
+console.log(empty);
+
+//Undefined
+//means there is no value assinged; not even an empty container; most importantly undefined is not an error.
+
+
+let undef = undefinded;
+console.log(udef);
+
+let grass;
+console.log(grass)://undefined;
+
+//Numbers
+   let degrees = 90;
+    console.log(degrees);
+
+JavaScript only has room for 15 numbers and then it begins to round up
+
+
+let numbersAreHard = (0.2 * 10 + 0.1 *10)
+
+
+//Strings
+///strings are datatypes that are used to represent text and are wrapped in either single or double quotes.
+
+let stringOne = "double quotes"
+let stringTwo = 'single quotes'
+
+console.log(stringOne, stringTwo);
+
+let first = 1050 + 100;
+let second = '1050 + 100';
+
+console.log(first);
+console.log(second); 
+
+//objects
+
+let car = {
+    coloer: 'red',
+    tires: 4,
+    extras: "A/C and Radio",
+    cool: true
+}
+
+console.log(car); 
+console.log(typeof car);
+
+//Arrays
+//Arrays are containers that hold lists of items;
+
+let list =[ 'item1', 'item2', 'item3'];
+//(1) (2) (3)
+
+//1 - name of the array;
+//2 - square brackets = array;
+//3 - each item, regardless of datatype, is separated by commas;
+
+let grocerylist = ['milk','apples', 'cereal'];
+console.log(grocerylist);
+console.log(typeof grocerylist); //object 
+
+//addition vs concatenation
+//check out javascript coersion
+
+
+let myInfo = {
+    firstName = 'Allan',
+    lastName = 'Halliburton',
+    houseNumber = '3743',
+    street = 'Roseway Lane',
+    city = 'Indidnapolis',
+    state = 'IN',
+    zipcode = '46226'
+}
+
+console.log(myInfo);
+
+let firstName = 'Allan'
+let lastName = 'Halliburton'
+let houseNumber = '3743'
+let street = 'Roseway Lane'
+let city = 'Indianapolis'
+let state = 'IN'
+
+console.log(firstName, lastName + ',' + houseNumber, street + ',', city, state);
+
+//Strings - Properties
+/*
+Properties are qualities associated with a dataType.
+Strings have properties associated with that string.
+E.g. the length of a string is a property.
+
+*/
+
+let myName = 'Allan';
+console.log(myName.length);
+
+//Methods
+//Methods are tools that can help us manipulkate our data.
+
+//*.property = no paraenthesis;
+//*.methods -parenthesis;
+
+let myNameIs = "Allan";
+console.log(myNameIs.toUpperCase())
+
+let home = 'My home is Indianapolis';
+console.log(home.includes(Indianapolis))
+
+let sent = 'This sentence will be split into individual parts';
+
+console.log(sent.split());
+console.log(sent.split(''))
